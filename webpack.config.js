e6dad17d7724d5804edd555f55b0a9f644aa89dc
Petter Astrup <petter.astrup@gmail.com>
2017-04-26 17:59:26 +0200

    Make Dependencies part of bundle

--- webpack.config.js
+++ webpack.config.js
@@ -41,8 +41,4 @@ module.exports = {
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
 };
