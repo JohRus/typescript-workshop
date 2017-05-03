b2ab618f933862270eab7be9a34f03563218506c
Johan Rusvik <johan.rusvik@bekk.no>
2017-05-03 18:18:23 +0200

    Legger til redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import reducer from './reducer/reducer';
 
-ReactDOM.render(
-    <App />,
+const store = createStore(reducer);
+
+const render = () => ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById("app")
 );
+
+render();
+store.subscribe(render);
---
+++ src/reducer/reducer.tsx
@@ -0,0 +1,8 @@
+
+
+export default (state: any = 0, action: any) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
