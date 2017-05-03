3df352485122d22e160357b449b20003e223f308
Steffen Hageland <steffen.hageland@bekk.no>
2017-05-03 18:05:55 +0200

    Skrur på “allowSyntheticDefaultImports”

--- src/components/Sok.tsx
+++ src/components/Sok.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { idrettsanleggApiType, idrettsanleggViewModel, idrettsanleggSearchResult } from '../types/idrettsanlegg';
 import {FormEvent} from "react";
 
