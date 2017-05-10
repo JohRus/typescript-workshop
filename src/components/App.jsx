0410e45c738eba0e59e72a9c0c928c009b61fc76
Steffen Hageland <steffen.hageland@bekk.no>
2017-05-10 18:56:09 +0200

    Gjør app.jsx til stateles-component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,12 @@ import Sok from './Sok';
 import Idrettsanleggliste from './Idrettsanleggliste';
 import Header from './Header';
 
-class App extends React.Component {
-    render() {
-        return (
-            <main className="app">
-                <Header />
-                <Sok />
-                <Idrettsanleggliste />
-            </main>
-        );
-    }
-}
+const App = () => (
+    <main className="app">
+        <Header />
+        <Sok />
+        <Idrettsanleggliste />
+    </main>
+);
+
 export default App;
